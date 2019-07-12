503c5783901db43d4542e3b7ea4a0614e01a9663
maodelong <mdl_android@163.com>
2019-07-13 01:29:18 +0800

    创建开发分支 commit

--- app/src/main/java/com/zfb/zhifabao/MainActivity.java
+++ app/src/main/java/com/zfb/zhifabao/MainActivity.java
@@ -3,6 +3,9 @@ package com.zfb.zhifabao;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 这是开发分支，用于智法宝开发
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
