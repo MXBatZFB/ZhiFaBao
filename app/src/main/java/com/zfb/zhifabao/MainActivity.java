31cbd600b3c3443c165601b1ca6f655062ce675b
maodelong <mdl_android@163.com>
2019-07-21 14:46:42 +0800

    test

--- app/src/main/java/com/zfb/zhifabao/MainActivity.java
+++ app/src/main/java/com/zfb/zhifabao/MainActivity.java
@@ -5,13 +5,11 @@ import com.zfb.zhifabao.common.app.Activity;
 /**
  * 这是开发分支，用于智法宝开发
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+    protected int getContentLayoutId() {
+        return R.layout.activity_main;
 
     }
 }
